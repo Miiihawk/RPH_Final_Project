eaeff550e1a28e9fb32dd6a68da4da11d0e43527
Miiihawk <131926009+Miiihawk@users.noreply.github.com>
2024-11-20 03:32:03 +0800

    created js file for county.html

---
+++ JavaScript/country.js
@@ -0,0 +1,13 @@
+var iconlyLightOutlineArrowL = document.getElementById("back-btn");
+if (iconlyLightOutlineArrowL) {
+    iconlyLightOutlineArrowL.addEventListener("click", function (e) {
+        window.location.href = "./index.html";
+    });
+}
+
+var byYearInfo8 = document.getElementById("byYearInfo8");
+if (byYearInfo8) {
+    byYearInfo8.addEventListener("click", function (e) {
+        window.location.href = "./by-year.html";
+    });
+}
