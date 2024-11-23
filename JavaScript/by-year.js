f852599c22858c496052e556646b1a175262ffea
Miiihawk <131926009+Miiihawk@users.noreply.github.com>
2024-11-23 15:58:20 +0800

    Merged files from testing ni alliza to testing and deleted some unnessesary files

--- JavaScript/by-year.js
+++
@@ -1,6 +0,0 @@
-var iconlyLightOutlineArrowL = document.getElementById("back-btn-timeline");
-if (iconlyLightOutlineArrowL) {
-    iconlyLightOutlineArrowL.addEventListener("click", function (e) {
-        window.location.href = "./country.html";
-    });
-}
